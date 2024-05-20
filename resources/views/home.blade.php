5a7d983ac80f00dee97e31a11e579542ca953875
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-20 15:42:36 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Ciao sono la homepage</h1>
+
+    <div >
+        ciao
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+<h1>ciao sono l'header</h1>
+
+
+
+</header>
