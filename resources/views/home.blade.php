179d0761ef3eaca18604c6960898bb555f345af8
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-20 17:52:06 +0200

    feat add card-section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="hero">
-        
-    </section>
+<section class="hero">
 
-    <section>
+</section>
 
-    </section>
+<section class="section-card-cinema">
+    <div class="container">
+
+        <div class="container-card">
+            @foreach ($cards as $card)
+            <div class="card">
+                <div class="container-img-card">
+                    <img src="{{$card['thumb']}}" alt="">
+                </div>
+                <h4>{{$card['series']}}</h4>
+            </div>
+            @endforeach
+
+        </div>
+    </div>
+</section>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $cards = config('comics');
+    return view('home', ['cards' => $cards]);
+  
 });
